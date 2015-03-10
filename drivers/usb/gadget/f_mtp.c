126154e4af32da0b78fb80f47bb0c1de09221bad
Jiebing Li <jiebing.li@intel.com>
2015-03-10 11:27:10 +0800

    ANDROID: usb: f_mtp: return error code if transfer error in receive_file_work function
    receive_file_work() function should report error if it detect the urb
    is not successfully transfered.
    
    Patchset: mtp
    
    Change-Id: I66898afe6b6ea2c33e6ea4c5ccf47d3a56d002dc
    Signed-off-by: Du, Changbin <changbin.du@gmail.com>
    Signed-off-by: Wang, Yu <yu.y.wang@intel.com>
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>

--- drivers/usb/gadget/f_mtp.c
+++ drivers/usb/gadget/f_mtp.c
@@ -805,6 +805,26 @@ static void receive_file_work(struct work_struct *data)
 					usb_ep_dequeue(dev->ep_out, read_req);
 				break;
 			}
+			if (dev->state == STATE_OFFLINE) {
+				r = -ENODEV;
+				if (!dev->rx_done)
+					usb_ep_dequeue(dev->ep_out, read_req);
+				break;
+			}
+			if (dev->state == STATE_RESET) {
+				DBG(cdev, "%s: DEVICE RESET\n", __func__);
+				r = -ECONNRESET;
+				if (!dev->rx_done) {
+					DBG(cdev, "dequeue in DEVICE RESET\n");
+					usb_ep_dequeue(dev->ep_out, read_req);
+				}
+				break;
+			}
+			if (read_req->status) {
+				r = read_req->status;
+				break;
+			}
+
 			/* if xfer_file_length is 0xFFFFFFFF, then we read until
 			 * we get a zero length packet
 			 */
